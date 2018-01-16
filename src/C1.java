1f868c282019087e3f15d45ff4afd6100aca27b0
devangswami <devangswami@LEC180373325266.trent.trentu.ca>
2018-01-15 19:30:07 -0500

    Added string

--- src/C1.java
+++ src/C1.java
@@ -4,7 +4,8 @@ public class C1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 10;
-		System.out.println("Hello World" + a);
+		String s = "Hello";
+		System.out.println("Hello World" + a + s);
 	
 	}
 
